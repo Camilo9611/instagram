c3e93510ecd51546c71bef24ba6d29dd78a68938
Camilo Ceballos <camiloacosta96@gmail.com>
2020-04-02 02:34:47 -0500

    Edicion de static root

--- instagram/settings.py
+++ instagram/settings.py
@@ -135,11 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,"alter"),
-)
-
 MEDIA_URL= '/media/'
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"alter")]
+STATIC_ROOT = os.path.join(BASE_DIR,"static_root")
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
