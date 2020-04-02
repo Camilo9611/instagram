3ad608e5c032fe12d1fb49745b5f7ab61a5b053e
Camilo Ceballos <camiloacosta96@gmail.com>
2020-04-02 02:46:08 -0500

    Adicion configuracion Template

--- instagram/settings.py
+++ instagram/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/Camilo Ceballos/Desktop/Proyecto_prueba/instagram/templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
